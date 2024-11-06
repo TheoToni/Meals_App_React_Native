b29cb735716151d31402761b50f6435e0d27dff9
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-06 16:26:50 +0100

    Filtering Meals with CategoryId

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,20 @@
 import { MEALS } from "../data/dummy-data";
-import { View, Text, StyleSheet } from "react-native";
+
+import { View, Text, StyleSheet, FlatList } from "react-native";
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((meal) =>
+    meal.categoryIds.includes(catId)
+  );
 
   return (
     <View style={styles.container}>
       <Text>{catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
     </View>
   );
 }
