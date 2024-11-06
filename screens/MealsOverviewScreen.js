0d585c2100cb1836261f5cc7333131da151462b9
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-06 16:53:14 +0100

    Filtering Meals 2

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,5 +1,5 @@
 import { MEALS } from "../data/dummy-data";
-
+import MealItem from "../components/MealItem";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
@@ -7,13 +7,17 @@ function MealsOverviewScreen({ route }) {
     meal.categoryIds.includes(catId)
   );
 
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>{catId}</Text>
       <FlatList
         data={displayedMeals}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={renderMealItem}
       />
     </View>
   );
