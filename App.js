24dce9df1f65ed1bb123d6cd10b0c95719d41cc2
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-07 15:24:28 +0100

    Dynamic title for subscreen

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
-            title: "All Categories",
             headerStyle: { backgroundColor: "#351401" },
             headerTintColor: "white",
             contentStyle: { backgroundColor: "#3f2f25" },
@@ -30,7 +29,16 @@ export default function App() {
               contentStyle: { backgroundColor: "#3f2f25" },
             }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            /* options={({ route, navigation }) => {
+              const catId = route.params.categoryId;
+              return {
+                title: catId,
+              };
+            }} */
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,19 @@
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 import { View, Text, StyleSheet, FlatList } from "react-native";
-function MealsOverviewScreen({ route }) {
+import { useLayoutEffect } from "react";
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
   const displayedMeals = MEALS.filter((meal) =>
     meal.categoryIds.includes(catId)
   );
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((cat) => cat.id === catId).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]); // Update the title when the categoryId changes
 
   function renderMealItem(itemData) {
     const item = itemData.item;
