8cd25d6015c6ddfa41cdd314bf4ea83aac43f79e
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-08 13:28:20 +0100

    navigate to meal detail screen 1

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,12 @@
+import { View, StyleSheet, Text } from "react-native";
+function MealDetailScreen() {
+  return (
+    <View>
+      <Text>Detailpage</Text>
+    </View>
+  );
+}
+
+export default MealDetailScreen;
+
+const styles = StyleSheet.create({});
