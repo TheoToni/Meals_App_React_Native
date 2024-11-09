c2985db247aa62790e7a6143aed7e8ef99d8783d
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-09 20:12:08 +0100

    Styling MealDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -6,16 +6,24 @@ function MealDetailScreen({ route }) {
   const meal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View>
-      <Image source={{ uri: meal.imageUrl }} />
-      <Text>{meal.title}</Text>
+      <Image style={styles.image} source={{ uri: meal.imageUrl }} />
+      <Text style={styles.title}>{meal.title}</Text>
       <MealDetails
         complexity={meal.complexity}
         affordability={meal.affordability}
         duration={meal.duration}
+        textStyle={styles.detailText}
       />
+      <View style={styles.subtitleContainer}>
+        <Text style={styles.subtitle}>Ingredients:</Text>
+      </View>
+
       {meal.ingredients.map((ingredient) => (
         <Text key={ingredient}>{ingredient}</Text>
       ))}
+      <View style={styles.subtitleContainer}>
+        <Text style={styles.subtitle}> Steps:</Text>
+      </View>
       {meal.steps.map((step) => (
         <Text key={step}>{step}</Text>
       ))}
@@ -26,6 +34,18 @@ function MealDetailScreen({ route }) {
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    margin: 8,
+    textAlign: "center",
+    Align: "center",
+    color: "white",
+  },
   details: {
     flexDirection: "row",
     alignItems: "center",
@@ -36,5 +56,26 @@ const styles = StyleSheet.create({
   detailItem: {
     marginHorizontal: 4,
     fontSize: 12,
+  },
+  detailText: {
+    color: "white",
+  },
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    borderBottomColor: "#e2b497",
+    borderBottomWidth: 2,
+    padding: 6,
+
+    marginHorizontal: 24,
+    marginVertical: 4,
+  },
+  text: {
+    color: "white",
   },
 });
